6979c2d7fbcb3f527418c61969a51a262723e8c7
dinara0708 <aikenova01@gmail.com>
2018-07-14 19:26:22 +0600

    -

--- LAYOUT - 3/Source.cpp
+++ LAYOUT - 3/Source.cpp
@@ -13,66 +13,66 @@ int main()
 
 	int tn = 0;
 
-	while (true)
+while (true)
+{
+	cout << "Задача № ";
+	cin >> tn;
+
+	switch (tn)
 	{
-		cout << "Задача № ";
-		cin >> tn;
+	case 1:
+	{
 
-		switch (tn)
-		{
-		case 1:
-		{
+	}
+	break;
+	case 2:
+	{
 
-		}
-		break;
-		case 2:
-		{
+	}
+	break;
+	case 3:
+	{
 
-		}
-		break;
-		case 3:
-		{
+	}
+	break;
+	case 4:
+	{
 
-		}
-		break;
-		case 4:
-		{
+	}
+	break;
+	case 5:
+	{
 
-		}
-		break;
-		case 5:
-		{
+	}
+	break;
+	case 6:
+	{
 
-		}
-		break;
-		case 6:
-		{
+	}
+	break;
+	case 7:
+	{
 
-		}
-		break;
-		case 7:
-		{
+	}
+	break;
+	case 8:
+	{
 
-		}
-		break;
-		case 8:
-		{
+	}
+	break;
+	case 9:
+	{
 
-		}
-		break;
-		case 9:
-		{
+	}
+	break;
+	case 10:
+	{
 
-		}
-		break;
-		case 10:
-		{
-
-		}
-		break;
-		default:
-			cout << "нет такой задачи" << endl << endl;
-		}
+	}
+	break;
+	default:
+		cout << "нет такой задачи" << endl << endl;
+	}
 
 	}
 	system("pause");
